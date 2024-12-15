6f668e969427ac2c226086621c1025ced4248b74
lappavel <wizd@tut.by>
2024-12-15 22:05:21 +0300

    refactor(extended-1.2): added alt for img

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="tasks-button tasks-button__delete";
   deleteButtonImg.className='tasks-button__delete-img';
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.alt='remove';
   deleteButton.appendChild(deleteButtonImg);
 
 
